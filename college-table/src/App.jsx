f12cb94cfa2109aae43a5b8a7852da3ef616057a
anudeeps0306 <anudeep0306@gmail.com>
2024-06-10 22:05:39 +0530

    feat: added analytics

--- college-table/src/App.jsx
+++ college-table/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { Analytics } from '@vercel/analytics/react';
 import CollegeTable from './component/CollegeTable';
+import ReactGa from 'react-ga4';
+import { useEffect } from 'react';
+
+const TRACKING_ID = 'G-5RHB2YQFNR';
+ReactGa.initialize(TRACKING_ID);
 
 function App() {
 
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+  
+
   return (
     <div className="App">
+      <Analytics />
       <CollegeTable />
     </div>
   )
