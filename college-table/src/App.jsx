8a7ae0144a487e06ad20d3eb51ba10a1037dc0fb
anudeeps0306 <anudeep0306@gmail.com>
2024-06-10 22:13:43 +0530

    chore: small changes

--- college-table/src/App.jsx
+++ college-table/src/App.jsx
@@ -1,22 +1,14 @@
 import './App.css'
-import { Analytics } from '@vercel/analytics/react';
 import CollegeTable from './component/CollegeTable';
-import ReactGa from 'react-ga';
-import { useEffect } from 'react';
+import ReactGa from 'react-ga4';
 
 const TRACKING_ID = 'G-5RHB2YQFNR';
 ReactGa.initialize(TRACKING_ID);
 
 function App() {
 
-  useEffect(() => {
-    ReactGa.pageview(window.location.pathname + window.location.search)
-  }, [])
-  
-
   return (
     <div className="App">
-      <Analytics />
       <CollegeTable />
     </div>
   )
